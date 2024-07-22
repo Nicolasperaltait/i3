e237c683b33683ae1373018c29c2eac162882d63
NicolasperaltaIT <nicolasperaltait@gmail.com>
2024-07-22 01:28:02 -0300

    .

--- Zsh.sh
+++ Zsh.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+sudo apt update && sudo apt upgrade -y
 
+sudo apt install zsh curl -y
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
