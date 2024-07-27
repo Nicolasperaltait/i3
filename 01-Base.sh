03ec3049d289a97290dbad184dd0be97db8d7ed6
NicolasperaltaIT <nicolasperaltait@gmail.com>
2024-07-27 02:25:33 -0300

    polybar + Configs

--- 01-Base.sh
+++ 01-Base.sh
@@ -8,11 +8,11 @@ setxkbmap 'us(intl)'                                                            
 
 sudo apt install nala -y && sudo nala fetch --auto -y                                                           # Examina los servidores espejo y setea los 3 primeros por defecto.
 
-sudo nala install xorg i3 i3status lxpolkit ntfs-3g dunst suckless-tools linux-headers-amd64 xfce4-session -y   # Esenciales para i3
+sudo apt install xorg i3 i3status lxpolkit ntfs-3g dunst suckless-tools linux-headers-amd64 xfce4-session polybar -y   # Esenciales para i3
 
-sudo nala install ufw clamav git wget curl zsh htop preload kitty flameshot xrandr rofi compton font-manager lxappearance timeshift -y     
+sudo apt install ufw clamav git wget curl zsh htop preload kitty flameshot xrandr rofi compton font-manager lxappearance timeshift -y     
 
-sudo nala install feh lxappearance numlockx arc-theme gammy solaar thunar gnome-calculator gnome-calendar obs-studio copyQ -y  #papirus-icon-theme // copyQ = clipbord manager // solaar = Control de mouse para el MX3          
+sudo apt install feh lxappearance numlockx arc-theme gammy solaar thunar gnome-calculator gnome-calendar obs-studio copyQ -y  #papirus-icon-theme // copyQ = clipbord manager // solaar = Control de mouse para el MX3          
 
 ## Gestion de Audio y Sonido 
 
