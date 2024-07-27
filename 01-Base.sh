6a40951e33d2b4f1a460def27e3191792a40ed3e
NicolasperaltaIT <nicolasperaltait@gmail.com>
2024-07-27 19:20:32 -0300

    fix

--- 01-Base.sh
+++ 01-Base.sh
@@ -6,13 +6,17 @@ sudo apt-get install software-properties-common -y && sudo apt-add-repository co
 setxkbmap 'us(intl)'                                                                                            # Teclado en Ingles Internacional con teclas muerta (AltGR)
 ## Instalaciones 
 
-sudo apt install nala -y && sudo nala fetch --auto -y                                                           # Examina los servidores espejo y setea los 3 primeros por defecto.
+#sudo apt install nala -y && sudo nala fetch --auto -y                                                           # Examina los servidores espejo y setea los 3 primeros por defecto.
 
-sudo apt install xorg i3 i3status lxpolkit ntfs-3g dunst suckless-tools linux-headers-amd64 xfce4-session polybar -y   # Esenciales para i3
+sudo apt install xorg i3 i3status dunst suckless-tools linux-headers-amd64 polybar -y   # Esenciales para i3
 
-sudo apt install ufw clamav git wget curl zsh htop preload kitty rofi compton font-manager lxappearance timeshift xrandr flameshot -y     
+sudo apt install lxpolkit ntfs-3g xfce4-session
 
-sudo apt install feh lxappearance numlockx arc-theme gammy solaar thunar gnome-calculator gnome-calendar obs-studio copyQ -y  #papirus-icon-theme // copyQ = clipbord manager // solaar = Control de mouse para el MX3          
+sudo apt install ufw clamav git wget curl zsh htop preload kitty rofi compton font-manager lxappearance timeshift  flameshot -y     
+
+sudo apt install feh lxappearance numlockx arc-theme  solaar thunar gnome-calculator gnome-calendar obs-studio  -y  #papirus-icon-theme // copyQ = clipbord manager // solaar = Control de mouse para el MX3          
+
+sudo apt install xrandr gammy copyQ
 
 ## Gestion de Audio y Sonido 
 
